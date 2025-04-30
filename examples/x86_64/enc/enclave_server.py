89001f341621a5219dc9af04e759a498a96b8029
srajendr <selvakumar.rajendran@nagra.com>
2025-04-30 10:48:48 +0200

    New Dockerfile

--- examples/x86_64/enc/enclave_server.py
+++ examples/x86_64/enc/enclave_server.py
@@ -1,21 +1,22 @@
 import boto3
 from botocore.config import Config
 
-# This config uses the vsock-proxy running on port 8000
+# Only valid config keys in Config constructor
 config = Config(
     region_name='us-east-1',
-    retries={'max_attempts': 3},
-    endpoint_url='http://127.0.0.1:8000',  # vsock proxy to KMS
-    signature_version='v4'
+    retries={'max_attempts': 3}
 )
 
-# Create a KMS client
-kms = boto3.client('kms', config=config)
+# Specify the vsock proxy endpoint here
+kms = boto3.client(
+    'kms',
+    endpoint_url='http://127.0.0.1:8000',  # Local vsock proxy to KMS
+    config=config
+)
 
-# Example: Encrypt something
 response = kms.encrypt(
     KeyId='alias/mrk-965d65eaa5cb4ca2b49cea0bea3ae2e1',
     Plaintext=b'hello enclave'
 )
 
-print("Ciphertext blob:", response['CiphertextBlob'])
+print("Encrypted:", response['CiphertextBlob'])
