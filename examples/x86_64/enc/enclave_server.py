1348e09c7d4721eae7fdf61d57e0bdbb397ab4ef
srajendr <selvakumar.rajendran@nagra.com>
2025-04-30 10:43:24 +0200

    New Dockerfile

--- examples/x86_64/enc/enclave_server.py
+++ examples/x86_64/enc/enclave_server.py
@@ -1,25 +1,21 @@
-from aws_kms import AwsKmsClient
-import base64
+import boto3
+from botocore.config import Config
 
-# Port should match the port where kms-server is running (on the host)
-VSOCK_PORT = 8001  # default port for kms-server
-KEY_ID = "arn:aws:kms:us-east-1:798842239772:key/mrk-965d65eaa5cb4ca2b49cea0bea3ae2e1"
+# This config uses the vsock-proxy running on port 8000
+config = Config(
+    region_name='us-east-1',
+    retries={'max_attempts': 3},
+    endpoint_url='http://127.0.0.1:8000',  # vsock proxy to KMS
+    signature_version='v4'
+)
 
-def main():
-    # Connect to KMS server on host via vsock
-    client = AwsKmsClient(vsock_port=VSOCK_PORT)
+# Create a KMS client
+kms = boto3.client('kms', config=config)
 
-    # Request to generate a data key
-    print("[*] Requesting data key from KMS...")
-    response = client.generate_data_key(key_id=KEY_ID, key_spec='AES_256')
+# Example: Encrypt something
+response = kms.encrypt(
+    KeyId='alias/mrk-965d65eaa5cb4ca2b49cea0bea3ae2e1',
+    Plaintext=b'hello enclave'
+)
 
-    # Encrypted key (can be stored)
-    encrypted_key_b64 = base64.b64encode(response['CiphertextBlob']).decode()
-    print("[+] Encrypted data key (base64):", encrypted_key_b64)
-
-    # Plaintext key (use this for actual encryption, but don’t store it!)
-    plaintext_key = base64.b64encode(response['Plaintext']).decode()
-    print("[+] Plaintext data key (base64):", plaintext_key)
-
-if __name__ == "__main__":
-    main()
+print("Ciphertext blob:", response['CiphertextBlob'])
